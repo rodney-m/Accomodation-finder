a2d5c7515707168287acddebd8ff6af6c635507f
Rodney Mupanduki <55084081+rodney-m@users.noreply.github.com>
2021-04-22 09:42:16 +0200

    added oncampus page

--- src/pages/OnCampus.js
+++ src/pages/OnCampus.js
@@ -1,8 +1,94 @@
 import React from "react";
 import "./Oncampus.css";
 
+import { Link } from "react-router-dom";
+
+import hostel from "../images/hostel.jpeg";
+
+import { IoMan, IoWoman } from "react-icons/io5";
+
 function OnCampus() {
-  return <div className="OnCampus">oncampus</div>;
+  return (
+    <div className="OnCampus">
+      <div className="container">
+        <Link to="rooms/single-room">
+          <div className="hostel">
+            <div className="hostelThumbnail">
+              <img src={hostel} alt="" />
+            </div>
+            <div className="hostelDetails">
+              <h3 className="title">Hostel 1</h3>
+              <p className="gender">
+                <IoMan /> <IoWoman /> Men & Women
+              </p>
+              <p className="details">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+              <h3 className="price">$10,000 zwl / Semester</h3>
+            </div>
+          </div>
+        </Link>
+
+        <Link to="rooms/single-room">
+          <div className="hostel">
+            <div className="hostelThumbnail">
+              <img src={hostel} alt="" />
+            </div>
+            <div className="hostelDetails">
+              <h3 className="title">Hostel 2</h3>
+              <p className="gender">
+                <IoMan /> Men
+              </p>
+              <p className="details">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+              <h3 className="price">$8,000 zwl / Semester</h3>
+            </div>
+          </div>
+        </Link>
+
+        <Link to="rooms/single-room">
+          <div className="hostel">
+            <div className="hostelThumbnail">
+              <img src={hostel} alt="" />
+            </div>
+            <div className="hostelDetails">
+              <h3 className="title">Hostel 3</h3>
+              <p className="gender">
+                <IoMan /> Men
+              </p>
+              <p className="details">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+              <h3 className="price">$8,000 zwl / Semester</h3>
+            </div>
+          </div>
+        </Link>
+
+        <Link to="rooms/single-room">
+          <div className="hostel">
+            <div className="hostelThumbnail">
+              <img src={hostel} alt="" />
+            </div>
+            <div className="hostelDetails">
+              <h3 className="title">Hostel 4</h3>
+              <p className="gender">
+                <IoWoman /> Women
+              </p>
+              <p className="details">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+              <h3 className="price">$8,000 zwl / Semester</h3>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default OnCampus;
