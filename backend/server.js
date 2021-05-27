0a134d7dd65ecc1d3b60a5816783dfedaf9e5dc5
Stan13y <46853837+iamstan13y@users.noreply.github.com>
2021-05-27 23:03:52 +0200

    Configured the backend and database connectivity.

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.DB_URI;
+mongoose.connect(uri, {
+    userNewUrlParser: true,
+    useCreateIndex: true
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Connection to database established successfully!');
+    ss
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
