90a1dc6ce9863dcd3902e427a25c8b6d8f65dbf4
Lionnel Tsuro <55039869+LionnelT@users.noreply.github.com>
2021-06-16 09:44:07 +0200

    Updated Contact Form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import "./Contact.css";
 
 function contact() {
     return (
-        <div>
-            <h1> contact</h1>
-        </div>
+        <div class="container">  
+  <form id="contact" action="" method="post">
+    <h3>AccoFinder Contact Form</h3>
+    <h4>Contact us for queries</h4>
+    <fieldset>
+      <input placeholder="Your name" type="text" tabindex="1" required autofocus/>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Email Address" type="email" tabindex="2" required/>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required/>
+    </fieldset>
+    <fieldset>
+      <textarea placeholder="Type your message here...." tabindex="5" required></textarea>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+  </form>
+</div>
+
     )
 }
 
