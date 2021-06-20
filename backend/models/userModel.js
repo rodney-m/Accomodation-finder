2e759b7b07f6dd90c9804ad9ab4409fd60264216
Tiondesol <79852165+Tiondesol@users.noreply.github.com>
2021-06-20 15:19:36 +0200

    created finance and owner model and updated user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,21 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new Schema({
-    given_name: String,
-    family_name: String,
-    nickname: String,
-    name: String,
-
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-
-
-    timestamps: true,
+    first_name: String,
+    lastname: String,
+    reg_number: String,
+    email:String  
 });
 
-const User = mongoose.model('User', userSchema);
+const user = mongoose.model('User', userSchema);
 
-module.exports = User;
+module.exports = user;
