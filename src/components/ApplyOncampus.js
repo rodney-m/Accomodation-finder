9bbd7d3e43eaf80f6148df684d4249916ee6498d
Rodney Mupanduki <rodneymupanduki@gmail.com>
2021-06-23 12:51:00 +0300

    changed icons on apply on campus

--- src/components/ApplyOncampus.js
+++ src/components/ApplyOncampus.js
@@ -1,37 +1,52 @@
 import React, { useState } from "react";
 import "./ApplyOncampus.css";
 import HostelsImg from "../images/hostels.jpg";
-import { FcBusinessman, FcBusinesswoman } from "react-icons/fc";
+import { ImManWoman, ImMan, ImWoman} from "react-icons/im";
 import {Link} from "react-router-dom"
 import ViewHostel from "./ViewHostel";
+import WifiIcon from '@material-ui/icons/Wifi';
+import VideocamIcon from '@material-ui/icons/Videocam';
 
 function ApplyOnCampus() {
   const [currentIndex, setCurrentIndex] = useState(null);
 
+  
+
   const Hostels = [
     {
       name: "Hostel 1",
-      gender: <FcBusinessman />,
-      gender2: <FcBusinesswoman />,
+      gender: <ImManWoman/>,
       prize: 8000,
+      amenities: [
+        {
+          name: "wifi", 
+          icon : <WifiIcon />,
+        }, 
+        {
+          name: "cctv",
+          icon : <VideocamIcon />,
+        }
+      ]
+    
     },
     {
       name: "Hostel 2",
-      gender: <FcBusinessman />,
+      gender: <ImMan />,
       prize: 6000,
     },
     {
       name: "Hostel 3",
-      gender: <FcBusinessman />,
+      gender: <ImMan />,
       prize: 6000,
     },
     {
       name: "Hostel 4",
-      gender: <FcBusinesswoman />,
+      gender: <ImWoman/>,
       prize: 6000,
     },
   ];
   return (
+    < div className="relative">
     <div className="ApplyOncampus">
       {Hostels.map((hostel, index) => {
         return (
@@ -59,8 +74,11 @@ function ApplyOnCampus() {
       })}
 
      
-      {currentIndex !== null ? <ViewHostel hostel={Hostels[currentIndex]} setCurrentIndex={setCurrentIndex}/> : ""}
     </div>
+    {currentIndex !== null ? <ViewHostel hostel={Hostels[currentIndex]} setCurrentIndex={setCurrentIndex}/> : ""}
+
+    </div>
+    
   );
 }
 
--- src/components/ViewHostel.js
+++ src/components/ViewHostel.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import "./ViewHostel.css"
+import React from "react";
+import "./ViewHostel.css";
+import CloseIcon from "@material-ui/icons/Close";
+import { IconButton } from "@material-ui/core";
+import { GiCctvCamera } from "react-icons/gi";
+import { FaWifi } from "react-icons/fa";
 
-function ViewHostel({hostel}) {
-    return (
-        <div className="ViewHostel">
-            <h1>{hostel.name}</h1>
+function ViewHostel({ hostel, setCurrentIndex }) {
+  return (
+    <div className="ViewHostel">
+      <span className="close">
+        <IconButton onClick={() => setCurrentIndex(null)}>
+          <CloseIcon className="closeBtn" />
+        </IconButton>
+      </span>
+      <div className="viewHostelBody">
+        <div className="text">
+          <h1>{hostel.name}</h1>
+          <p>
+            <span className="amenity">
+              WiFi <FaWifi />
+            </span>
+            <span className="amenity">
+              CCTV <GiCctvCamera />
+            </span>
+          </p>
+          {hostel.name === "Hostel 2" ||
+          hostel.name === "Hostel 3" ||
+          hostel.name === "Hostel 4" ? (
+            <h1>$ 6000</h1>
+          ) : (
+            <h1>$ 8000</h1>
+          )}
+          <IconButton>
+            <span className="btn">Apply</span>
+          </IconButton>
         </div>
-    )
+
+        <div className="images"></div>
+      </div>
+    </div>
+  );
 }
 
-export default ViewHostel
+export default ViewHostel;
