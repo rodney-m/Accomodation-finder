d1205c00f74818c15ece23d4816e0ecd17f285bb
= <=>
2021-06-30 20:14:50 +0200

    Updated Offcampus.js to show listed houses.

--- src/components/Offcampus.js
+++ src/components/Offcampus.js
@@ -1,52 +1,62 @@
-import React from 'react'
-import "./Offcampus.css"
+import React, { useState, useEffect } from "react";
+import "./Offcampus.css";
 import InfoIcon from "@material-ui/icons/Info";
-import {IconButton} from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
+import axios from 'axios';
 
 function Offcampus() {
-    const houses = [
-        {
-          name: "John Doe",
-          address: "67 Skymaster Belvedere",
-        },
-        {
-          name: "Jane Doe",
-          address: "34 Ganges Road",
-        },
-        {
-          name: "John Smith",
-          address: "28 Ganges Road",
-        },
-      ];
-    return (
-        <div className="Offcampus">
-            <h1>Off Campus</h1>
-            <table cellSpacing="0">
-        <thead>
-          <tr>
-            <td>Owner</td>
-            <td>Address</td>
-            <td></td>
-          </tr>
-        </thead>
-        <tbody>
-          {houses.map((house) => {
-            return (
-              <tr>
-                <td>{house.name}</td>
-                <td>{house.address}</td>
-                <td>
-                  <IconButton>
-                    <InfoIcon className="info" />
-                  </IconButton>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-        </div>
+
+    const [houses, setHouses] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/offcampus/')
+            .then(response => {
+                setHouses(response.data.filter(data => data.approved === true))
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+    })
+
+    return ( <
+        div className = "Offcampus" >
+        <
+        h1 > Off Campus < /h1> <
+        table cellSpacing = "0" >
+        <
+        thead >
+        <
+        tr >
+        <
+        td > Owner < /td> <
+        td > Address < /td> <
+        td > < /td> < /
+        tr > <
+        /thead> <
+        tbody > {
+            houses.map((house) => {
+                return ( <
+                    tr >
+                    <
+                    td > { house.owner } < /td> <
+                    td > { house.address } < /td> <
+                    td >
+                    <
+                    IconButton >
+                    <
+                    InfoIcon className = "info" / >
+                    <
+                    /IconButton> < /
+                    td > <
+                    /tr>
+                );
+            })
+        } <
+        /tbody> < /
+        table > <
+        /div>
     )
 }
 
-export default Offcampus
+export default Offcampus;
