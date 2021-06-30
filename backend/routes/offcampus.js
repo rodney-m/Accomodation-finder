19c2e75f3b7c11b09f35d999fffc55ccaf40f5b2
Stan13y <46853837+iamstan13y@users.noreply.github.com>
2021-06-30 18:16:32 +0200

    Updated Off Campus route.

--- backend/routes/offcampus.js
+++ backend/routes/offcampus.js
@@ -1,9 +1,37 @@
 const router = require('express').Router();
-let OffCampus = require('../models/hostelModel');
+let House = require('../models/offCampusModel');
 
 router.route('/').get((req, res) => {
-    OffCampus.find()
+    House.find()
         .then(houses => res.json(houses))
+        .catch(error => res.status(400).json('Error:' + error));
+});
+
+router.route('/add').post((req, res) => {
+    const owner = req.body.owner;
+    const address = req.body.address;
+    const phone_number = req.body.phone_number;
+    const gender = req.body.gender;
+    const beds = Number(req.body.beds);
+    const cost = req.body.cost;
+    const amenities = req.body.amenities;
+    const images = req.body.images;
+    const approved = req.body.approved;
+
+    const newHouse = new House({
+        owner,
+        address,
+        phone_number,
+        gender,
+        beds,
+        cost,
+        amenities,
+        images,
+        approved
+    });
+
+    newHouse.save()
+        .then(() => res.json('House added.'))
         .catch(error => res.status(400).json('Error:' + error));
 });
 
