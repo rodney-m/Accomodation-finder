a87032f15ae3cfcddb8ec343c99eb9a2297ffdd8
= <=>
2021-07-02 00:41:39 +0200

    Created Controller for uploading Images.

---
+++ backend/controllers/upload.js
@@ -0,0 +1,21 @@
+const upload = require("../middleware/upload");
+
+const uploadFile = async (req, res) => {
+  try {
+    await upload(req, res);
+
+    console.log(req.file);
+    if (req.file == undefined) {
+      return res.send(`Select a file to upload.`);
+    }
+
+    return res.send(`File has been uploaded.`);
+  } catch (error) {
+    console.log(error);
+    return res.send(`Error when trying upload image: ${error}`);
+  }
+};
+
+module.exports = {
+  uploadFile: uploadFile
+};
--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -3,21 +3,21 @@ const multer = require("multer");
 const GridFsStorage = require("multer-gridfs-storage");
 
 var storage = new GridFsStorage({
-      url: process.env.DB_URI,
-      options: { useNewUrlParser: true, useUnifiedTopology: true },
-      file: (req, file) => {
-            const match = ["image/png", "image/jpeg"];
+    url: process.env.DB_URI,
+    options: { useNewUrlParser: true, useUnifiedTopology: true },
+    file: (req, file) => {
+        const match = ["image/png", "image/jpeg"];
 
-            if (match.indexOf(file.mimetype) === -1) {
-                  const filename = `${Date.now()}-accofinder-${file.originalname}`;
-                  return filename;
-            }
+        if (match.indexOf(file.mimetype) === -1) {
+            const filename = `${Date.now()}-accofinder-${file.originalname}`;
+            return filename;
+        }
 
-            return {
-                  bucketName: "photos",
-                  filename: `${Date.now()}-accofinder-${file.originalname}`
-            };
-      }
+        return {
+            bucketName: "photos",
+            filename: `${Date.now()}-accofinder-${file.originalname}`
+        };
+    }
 });
 
 var uploadFile = multer({ storage: storage }).single("file");
