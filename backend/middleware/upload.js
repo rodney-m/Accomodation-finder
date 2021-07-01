d48de79f8648ce100131c9979374632c8453fa2f
= <=>
2021-07-02 00:24:32 +0200

    Created middleware for uploading & storing images.

---
+++ backend/middleware/upload.js
@@ -0,0 +1,25 @@
+const util = require("util");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+
+var storage = new GridFsStorage({
+      url: process.env.DB_URI,
+      options: { useNewUrlParser: true, useUnifiedTopology: true },
+      file: (req, file) => {
+            const match = ["image/png", "image/jpeg"];
+
+            if (match.indexOf(file.mimetype) === -1) {
+                  const filename = `${Date.now()}-accofinder-${file.originalname}`;
+                  return filename;
+            }
+
+            return {
+                  bucketName: "photos",
+                  filename: `${Date.now()}-accofinder-${file.originalname}`
+            };
+      }
+});
+
+var uploadFile = multer({ storage: storage }).single("file");
+var uploadFilesMiddleware = util.promisify(uploadFile);
+module.exports = uploadFilesMiddleware;
