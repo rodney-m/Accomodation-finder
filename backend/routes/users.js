e2a6de5643a60e4216af50726a6032e4a48d645d
iamstan13y <iamstan13y@gmail.com>
2021-07-19 21:53:13 +0200

    user login and sign up, in progress.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -75,35 +75,5 @@ router.route('/login').post((req, res, next) => {
                 console.log(err);
             });
     })
-    // router.route('/add').post((req, res) => {
-
-//     const given_name = req.body.given_name;
-//     const family_name = req.body.family_name;
-//     const nickname = req.body.nickname;
-//     const name = req.body.name;
-//     const picture = req.body.picture;
-//     const locale = req.body.locale;
-//     const updated_at = req.body.updated_at;
-//     const email = req.body.email;
-//     const email_verified = req.body.email_verified;
-//     const sub = req.body.sub;
-
-//     const newUser = new User({
-//         given_name,
-//         family_name,
-//         nickname,
-//         name,
-//         picture,
-//         locale,
-//         updated_at,
-//         email,
-//         email_verified,
-//         sub,
-//     });
-
-//     newUser.save()
-//         .then(() => res.json('User added.'))
-//         .catch(error => res.status(400).json('Error:' + error));
-// });
 
 module.exports = router;
