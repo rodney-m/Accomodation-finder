5f6f61960fde8ad039fe4b5732429a9723eb8eb5
Keith Stanley <iamstan13y@gmail.com>
2021-07-24 22:15:43 +0200

    House owner dashboard purring like a kitten.

--- backend/models/applicationsModel.js
+++ backend/models/applicationsModel.js
@@ -7,10 +7,13 @@ const applicationSchema = new Schema({
     fname: { type: String, required: true },
     lname: { type: String, required: true },
     year: { type: String, required: true },
-    hostel: { type: Number, required: true },
-    room_no: { type: Number, required: true },
     department: { type: String, required: true },
-    status: { type: Boolean}
+    hostel: { type: Number },
+    room_no: { type: Number },
+    phone: { type: String },
+    email: { type: String },
+    message: { type: String },
+    status: { type: Boolean }
 });
 
 const Application = mongoose.model('Application', applicationSchema);
