0e9f399cfb4b002a2d4860f2c588f6447b407bbb
Keith Stanley <iamstan13y@gmail.com>
2021-07-25 10:53:38 +0200

    added sign up logic.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,39 +1,45 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
 let User = require('../models/userModel');
+let Student = require('../models/studentModel');
 const jwt = require('jsonwebtoken');
 
 router.route('/signup').post((req, res, next) => {
-    User.find({ email: req.body.email })
-        .exec()
-        .then(user => {
-
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                if (err) {
-                    return res.status(500).json({
-                        error: err
-                    });
-                } else {
-                    const user = new User({
-                        reg_number: req.body.reg_number,
-                        email: req.body.email,
-                        password: hash
-                    });
-                    user
-                        .save()
-                        .then(result => {
-                            res.status(201).json({
-                                message: 'User created.'
-                            });
-                        })
-                        .catch(err => {
-                            console.log(err);
-                            res.status(500).json({
-                                error: err
-                            })
+    Student.find({ regNumber: req.body.reg_number })
+        .then(student => {
+            if (!student) {
+                return res.status(404).json({
+                    error: "Not found"
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
                         });
-                }
-            });
+                    } else {
+                        const user = new User({
+                            reg_number: req.body.reg_number,
+                            fullName: student.firstNames + " " + student.lastName,
+                            email: student.email,
+                            password: hash
+                        });
+                        user
+                            .save()
+                            .then(result => {
+                                res.status(200).json({
+                                    message: 'User created.'
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                })
+                            });
+                    }
+                });
+            }
         })
         .catch(err => {
             console.log(err);
