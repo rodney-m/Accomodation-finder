79678b69e4f2b8b07f0bcd5aa266939c83ee4f5b
Keith Stanley <iamstan13y@gmail.com>
2021-07-25 11:28:21 +0200

    Added routes for student details.

---
+++ backend/routes/students.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+let Student = require('../models/studentModel');
+
+router.route('/').get((req, res) => {
+    Student.find()
+        .then(students => res.json(students))
+        .catch(error => res.status(400).json('Error:' + error));
+});
+
+router.route('/add').post((req, res) => {
+    const regNumber = req.body.regNumber;
+    const firstNames = req.body.firstNames;
+    const lastName = req.body.lastName;
+    const address = req.body.address;
+    const phoneNumber = req.body.phoneNumber;
+    const year = req.body.year;
+    const dept = req.body.dept;
+    const paidTuition = Number(req.body.paidTuition);
+
+    const newStudent = new Student({
+        regNumber,
+        firstNames,
+        lastName,
+        address,
+        phoneNumber,
+        year,
+        dept,
+        paidTuition
+    });
+
+    newStudent.save()
+        .then(() => res.json('Student added.'))
+        .catch(error => res.status(400).json('Error:' + error));
+});
+
+module.exports = router;
